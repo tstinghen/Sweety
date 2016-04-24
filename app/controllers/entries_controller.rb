42e6841fb47adc3c0018635803c5d677d58dc710
tstinghen <tstinghen@gmail.com>
2016-04-24 10:19:20 -0700

    Added some clarifying comments

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,17 +3,21 @@ class EntriesController < ApplicationController
 
 
 	def index 
+	#keeping everything on the new page
 		redirect_to :action => 'new' 
 	end
 	
 	def show 
+	#keeping everything on the new page
 		redirect_to :action => 'new' 
 	end
 	  
 	def new 
 		@entry = Entry.new
+		#determines how many CBG a user can submit per day, change for testing 
 		#@max_daily = 400 #testing
 		@max_daily = 4
+		#generates a list of the daily results 
 		@daily_list = Entry.where('date >= ?' , Date.yesterday.end_of_day)
 		@daily_count = @daily_list.size
 
@@ -23,6 +27,7 @@ class EntriesController < ApplicationController
 	
 	def create 
 		@entry = Entry.new(entry_params)
+		#every CBG result is time stamped when submitted, instead of manually
 		@entry.date = DateTime.now
 		@entry.save 
 		redirect_to @entry
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,11 @@
 class ReportsController < ApplicationController
 
 def month
-
+	#parses results from the select_month selector
 	@select_month = sprintf('%02d', params[:date][:month]) 
-	@what_is_date = params[:date] #testing 
 	@month_name = Date::MONTHNAMES[@select_month.to_f]
+	#text matching to filter for month. 
+	#would be easy enough to put a year selector and match for that as well
 	@entry_list = Entry.where("strftime('%m', date) = ?", @select_month)	
 		entry_stats(@entry_list) 
 
@@ -13,13 +14,16 @@ end
 
 def monthtodate 
 	@select_month = Time.now
+	#text matching for month (could be converted to month/year) 
 	@entry_list = Entry.where("strftime('%m', date) = ?", Time.now.strftime("%m"))	
 		entry_stats(@entry_list) 
 end 
 
 
 def date 
+	#parses date from date_selector
 	@select_date = sprintf('%02d', params[:date][:month]) + '/'+ sprintf('%02d', params[:date][:day]) + '/'+ sprintf('%02d', params[:date][:year])
+	#text matching for entire date
 	@entry_list = Entry.where("strftime ('%m/%d/%Y', date) = ?", @select_date)	
 	entry_stats(@entry_list) 
 
@@ -27,6 +31,7 @@ def date
 end 
 
 def entry_stats(entry_list) 
+		#creates stats out of a populated array, or just sets min/max to 0 
 		if entry_list.size > 0 
 		
 			@entry_vals = entry_list.map {|entry| entry.cbg}
